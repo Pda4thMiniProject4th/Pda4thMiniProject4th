e328b5c9127b00c662f46d6bdc0e5159a7ab9b81
ymkdev <eveminful@gmail.com>
2024-05-27 03:00:31 +0900

    fix: Drawertest.js - db 변경 후 데이터 조회 버그 수정
    #26

--- src/test/components/myPage/Drawertest.js
+++ src/test/components/myPage/Drawertest.js
@@ -9,6 +9,22 @@ export default function Drawertest() {
   const [userprofile, setUserProfile] = useState("");
   const [seatOption, setSeatOption] = useState("");
 
+  const fetchUserInfo = async () => {
+    try {
+      const response = await axios.get("/mypage/2"); // idValue가 2인 예시
+      if (response.data) {
+        const { name, profile, seat_option } = response.data;
+        setUserName(name);
+        setUserProfile(profile);
+        if (seat_option === -1) setSeatOption("뒷자리");
+        else if (seat_option === 1) setSeatOption("앞자리");
+        else setSeatOption("랜덤");
+      }
+    } catch (error) {
+      console.error("Error fetching user info:", error);
+    }
+  };
+
   const toggleDrawer = (open) => (event) => {
     if (
       event.type === "keydown" &&
@@ -18,28 +34,10 @@ export default function Drawertest() {
     }
 
     setIsOpen(open);
+    if (open) {
+      fetchUserInfo();
+    }
   };
-
-  useEffect(() => {
-    const fetchUserInfo = async () => {
-      try {
-        const response = await axios.get("/mypage/1"); // idValue가 1인 예시
-        if (response.data) {
-          const { name, profile, seat_option } = response.data;
-          console.log(response.data);
-          setUserName(name);
-          setUserProfile(profile);
-          if (seat_option === -1) setSeatOption("뒷자리");
-          else if (seat_option === 1) setSeatOption("앞자리");
-          else setSeatOption("랜덤");
-        }
-      } catch (error) {
-        console.error("Error fetching fetchUserInfo:", error);
-      }
-    };
-
-    fetchUserInfo();
-  }, []);
 
   return (
     <div>
