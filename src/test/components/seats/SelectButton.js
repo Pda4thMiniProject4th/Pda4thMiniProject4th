4d80a3e953c67543c2988c9ad13689c5bf450146
ymkdev <eveminful@gmail.com>
2024-05-26 16:28:02 +0900

    feat: (SelectButton.js, Select.js) - 자리 Select UI, 확인 Button UI
    #23

---
+++ src/test/components/seats/SelectButton.js
@@ -0,0 +1,11 @@
+import Button from "react-bootstrap/Button";
+
+function SelectButton() {
+  return (
+    <>
+      <Button variant="info">확인</Button>{" "}
+    </>
+  );
+}
+
+export default SelectButton;
