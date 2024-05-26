94d201e66cb21c3403f838a3e11f86eb02da5f5c
ymkdev <eveminful@gmail.com>
2024-05-27 00:54:20 +0900

    feat: 마이페이지 API 연동 완료
    #25

--- src/test/components/myPage/Drawertest.js
+++ src/test/components/myPage/Drawertest.js
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
+import axios from "axios";
 
 export default function Drawertest() {
   const [isOpen, setIsOpen] = useState(false);
+  const [userName, setUserName] = useState("");
+  const [userprofile, setUserProfile] = useState("");
+  const [seatOption, setSeatOption] = useState("");
 
   const toggleDrawer = (open) => (event) => {
     if (
@@ -16,6 +20,27 @@ export default function Drawertest() {
     setIsOpen(open);
   };
 
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const response = await axios.get("/mypage/1"); // idValue가 1인 예시
+        if (response.data) {
+          const { name, profile, seat_option } = response.data;
+          console.log(response.data);
+          setUserName(name);
+          setUserProfile(profile);
+          if (seat_option === -1) setSeatOption("뒷자리");
+          else if (seat_option === 1) setSeatOption("앞자리");
+          else setSeatOption("랜덤");
+        }
+      } catch (error) {
+        console.error("Error fetching fetchUserInfo:", error);
+      }
+    };
+
+    fetchUserInfo();
+  }, []);
+
   return (
     <div>
       <Button onClick={toggleDrawer(true)}>마이페이지</Button>
@@ -26,7 +51,9 @@ export default function Drawertest() {
           onClick={toggleDrawer(false)}
           onKeyDown={toggleDrawer(false)}
         >
-          <p>마이페이지 프로필 확인페이지</p>
+          <p>프로필: {userprofile}</p>
+          <p>이름: {userName}</p>
+          <p>선택한 자리: {seatOption}</p>
           <p>로그아웃</p>
         </div>
       </Drawer>
