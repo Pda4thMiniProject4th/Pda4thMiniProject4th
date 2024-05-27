67db855e7d6c0b5677a813f26abff8a35dcf9482
ymkdev <eveminful@gmail.com>
2024-05-27 10:56:34 +0900

    feat: (SelectButton.js,Root2.js) - 자리 선택 및 확정 API 연동 테스트 코드
    #27

--- src/test/components/seats/SelectButton.js
+++ src/test/components/seats/SelectButton.js
@@ -1,11 +1,43 @@
+import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import axios from "axios";
+import { useState } from "react";
 
-function SelectButton() {
+const SelectButton = () => {
+  const [selectedOption, setSelectedOption] = useState(0);
+
+  const fetchUserInfo = async () => {
+    try {
+      const id = 2; //테스트
+      const reason = "질문해야해서"; //테스트
+      const response = await axios.post("/seats/live", {
+        id,
+        seat_option: selectedOption,
+        reason,
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <>
-      <Button variant="info">확인</Button>{" "}
-    </>
+    <div style={{ display: "flex" }}>
+      <Form.Select
+        aria-label="Default select example"
+        defaultValue={0}
+        style={{ width: "100px" }}
+        onChange={(e) => setSelectedOption(Number(e.target.value))}
+      >
+        <option value={0}>랜덤</option>
+        <option value={1}>앞자리</option>
+        <option value={-1}>뒷자리</option>
+      </Form.Select>
+      <Button variant="info" onClick={fetchUserInfo}>
+        확인
+      </Button>{" "}
+    </div>
   );
-}
+};
 
 export default SelectButton;
