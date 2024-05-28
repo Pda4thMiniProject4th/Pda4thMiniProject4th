a4a31d9721aae8128ebe29e60d24ac28dfc657aa
ymkdev <eveminful@gmail.com>
2024-05-28 14:20:04 +0900

    refactor: 실시간 좌석 코드 리팩토링
    #48

--- src/test/components/seats/Row2.js
+++ src/test/components/seats/Row2.js
@@ -1,16 +1,15 @@
 import React from "react";
 import { Row } from "react-bootstrap";
-import Seat2 from "./Seat2"; // 경로 확인 필요 (예: "./Seat")
-
+import Seat2 from "./Seat2";
 const SeatingRow2 = ({ seats }) => {
   return (
-    <Row noGutters>
+    <Row>
       {seats.map((seat, index) => (
         <Seat2
           key={index}
           size={2} // Bootstrap grid 시스템에서 총 12열 중 2의 크기 (6개 좌석)
-          occupied={seat.userName !== "Empty"}
-          number={seat.userName} // 사용자 이름을 number prop으로 전달,
+          occupied={seat.selected !== ""} // ""선택된게 아니라면 gray
+          isSelected={seat.selected} // 자리의 선택 여부
         />
       ))}
     </Row>
--- src/test/components/seats/SeatingChart_live.js
+++ src/test/components/seats/SeatingChart_live.js
@@ -13,7 +13,7 @@ const SeatingChart_live = ({ countData }) => {
   useEffect(() => {
     const fetchCount = async () => {
       try {
-        const response = await axios.get("/users/10"); //orders 10인 예시
+        const response = await axios.get("/users/3"); //orders 3인 예시
         if (response.data) {
           const { frontCount, backCount } = response.data;
           setFrontCount(frontCount);
@@ -35,9 +35,10 @@ const SeatingChart_live = ({ countData }) => {
 
   const initializeSeating = (frontCount, backCount) => {
     const newSeatingPlan = Array.from({ length: rows }, () =>
+      // 8*6
       Array.from({ length: seatsPerRow }, () => ({
-        occupied: false,
-        userName: "Empty",
+        occupied: false, //silver
+        selected: "", //빈좌석
       }))
     );
 
@@ -47,7 +48,7 @@ const SeatingChart_live = ({ countData }) => {
       const col = i % seatsPerRow;
       if (row < rows) {
         newSeatingPlan[row][col].occupied = true;
-        newSeatingPlan[row][col].userName = "X";
+        newSeatingPlan[row][col].selected = "X";
       }
     }
 
@@ -57,83 +58,20 @@ const SeatingChart_live = ({ countData }) => {
       const col = i % seatsPerRow;
       if (row >= 0) {
         newSeatingPlan[row][col].occupied = true;
-        newSeatingPlan[row][col].userName = "X";
+        newSeatingPlan[row][col].selected = "X";
       }
     }
 
     setSeatingPlan(newSeatingPlan);
   };
-  // const initializeSeating = (frontCount, backCount) => {
-  //   const newSeatingPlan = [];
-  //   for (let i = 0; i < rows; i++) {
-  //     const rowSeats = [];
-  //     for (let j = 0; j < seatsPerRow; j++) {
-  //       let occupied = false;
-  //       let userName = "";
-  //       // Apply front count to only the first seat of the first row(s)
-  //       if (i < frontCount && j === 0) {
-  //         userName = "X";
-  //         occupied = true;
-  //       }
-  //       // Apply back count to only the first seat of the last row(s)
-  //       if (i === 7 && j >= rows - backCount) {
-  //         userName = "X";
-  //         occupied = true;
-  //       }
-  //       rowSeats.push({
-  //         size: 1,
-  //         occupied: occupied,
-  //         userName: userName,
-  //       });
-  //     }
-  //     newSeatingPlan.push(rowSeats);
-  //   }
-  //   setSeatingPlan(newSeatingPlan);
-  // };
-  // 모든 행과 좌석 초기화(우선 임의로 ui만듬) => 아래에 주석 달은 코드(get요청 받아올 예정)
-  // for (let i = 0; i < rows; i++) {
-  //   const rowSeats = [];
-  //   for (let j = 0; j < seatsPerRow; j++) {
-  //     rowSeats.push({
-  //       size: 1,
-  //       occupied: false,
-  //     });
-  //   }
-  //   seatingPlan.push(rowSeats);
-  // }
 
   return (
     <Container>
       {seatingPlan.map((row, index) => (
-        <SeatingRow2
-          key={index}
-          seats={row}
-          // startNumber={index * seatsPerRow + 1}
-        />
+        <SeatingRow2 key={index} seats={row} />
       ))}
     </Container>
   );
 };
 
 export default SeatingChart_live;
-
-// 나중에 api로 get요청 받아와서 좌석 띄울 예정
-// const SeatingChart = () => {
-//   const [seatingPlan, setSeatingPlan] = useState([]); // 좌석 데이터를 관리할 상태 변수 초기화
-
-//   useEffect(() => {
-//     // 컴포넌트 마운트 시 데이터를 가져오는 효과
-//     const fetchSeats = async () => {
-//       try {
-//         // API 엔드포인트에서 좌석 데이터를 비동기적으로 요청
-//         const response = await axios.get('API_ENDPOINT');
-//         // 응답 데이터를 상태 변수에 저장
-//         setSeatingPlan(response.data);
-//       } catch (error) {
-//         // 데이터 요청 중 에러가 발생하면 콘솔에 로그 출력
-//         console.error('Error fetching seat data:', error);
-//       }
-//     };
-
-//     fetchSeats(); // 함수 호출
-//   }, []); // 의존성 배열을 비워서 컴포넌트 마운트 시에만 효과 실행
