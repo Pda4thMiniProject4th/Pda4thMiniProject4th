7fd7ffd9f13614f3319cd861c2121f7009301ffc
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2024-05-28 13:31:06 +0900

    fix: SeatingChart_current.js - 에러 발생이 우려되어 예전버전으로 돌림

--- src/test/components/seats/SeatingChart_current.js
+++ src/test/components/seats/SeatingChart_current.js
@@ -3,8 +3,9 @@ import axios from "axios";
 import { Container } from "react-bootstrap";
 import SeatingRow from "./Row";
 
-const SeatingChart_current = ({ orders }) => {
+const SeatingChart_current = () => {
   const [userNames, setUserNames] = useState([]); // 사용자 이름을 저장할 배열
+  const [loggedInUserId, setLoggedInUserId] = useState("2"); // ID: 2
   const rows = 8; // 총 8개의 행
   const seatsPerRow = 6; // 각 행당 6개의 좌석
 
@@ -12,8 +13,7 @@ const SeatingChart_current = ({ orders }) => {
     // 서버에서 사용자 이름을 가져오는 함수
     const fetchUserNames = async () => {
       try {
-        console.log({ orders });
-        const response = await axios.get(`/seats/current/${{ orders }.orders}`); // API 경로 수정 필요
+        const response = await axios.get("/seats/current/10"); // API 경로 수정 필요
         console.log(response.data);
         setUserNames(response.data); // 응답 데이터를 상태로 저장
       } catch (error) {
@@ -22,22 +22,23 @@ const SeatingChart_current = ({ orders }) => {
     };
 
     fetchUserNames(); // 함수 실행
-  }, [orders]);
+  }, []);
 
-  const seats = Array.from({ length: 48 }, (_, index) => ({
+  const seats = Array.from({ length: 49 }, (_, index) => ({
     seatNumber: index + 1,
     userName: userNames[index] || "Empty",
   }));
 
   const seatingPlan = [];
-  for (let i = 0; i < seats.length; i += seatsPerRow) {
+  for (let i = 1; i < seats.length; i += seatsPerRow) {
     seatingPlan.push(seats.slice(i, i + seatsPerRow));
   }
 
   return (
     <Container>
       {seatingPlan.map((row, index) => (
-        <SeatingRow key={index} seats={row} />
+        <SeatingRow key={index} seats={row} loggedInUserId={loggedInUserId} />
+        //loggedInUserId={"2"}
       ))}
     </Container>
   );
