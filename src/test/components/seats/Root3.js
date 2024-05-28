b96e91f43eec4a8a725e48676c4d4dff5160cd02
ymkdev <eveminful@gmail.com>
2024-05-28 14:57:33 +0900

    design: files - Root3 컴포넌트 재구성 및 css 테스트
    #40

--- src/test/components/seats/Root3.js
+++ src/test/components/seats/Root3.js
@@ -1,23 +1,25 @@
 import React, { useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
 import SeatingChart_current from "./SeatingChart_current";
 import SeatingChart_live from "./SeatingChart_live";
 import SelectButton from "./SelectButton";
+import "./Root3.css";
 
 export default function Root3() {
   const [countData, setCountData] = useState([]);
   return (
-    <div style={{ display: "flex" }}>
+    <div className="seat-current-live" style={{ display: "flex" }}>
       <div>
         <h3>현재 자리</h3>
         <SeatingChart_current />
       </div>
-      <div>
-        <div style={{ display: "flex" }}>
-          <h3>실시간 자리 현황</h3>
-          <SelectButton onDataFetched={setCountData} />
+      <div className="live-set">
+        <div style={{ display: "flex" }} className="live-title">
+          <h4>실시간 자리 현황</h4>
+          <SelectButton onDataFetched={setCountData} /> {/*id, 기수*/}
         </div>
-        <SeatingChart_live countData={countData} />
+        <div className="live-seat">
+          <SeatingChart_live countData={countData} /> {/*id, 기수*/}
+        </div>
       </div>
     </div>
   );
