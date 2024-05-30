c54cef30e41efcc65ad7db4835d64cc3d7655d95
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2024-05-30 12:37:28 +0900

    feat: Row.js - css 적용
    #80

--- src/admin/seatArrange/components/Row.js
+++ src/admin/seatArrange/components/Row.js
@@ -3,19 +3,53 @@ import { Row } from "react-bootstrap";
 import Seat from "./Seat";
 
 export default function SeatingRow({ seats, meta, setMeta, orders }) {
+  console.log(seats);
+
+  const rows = [];
+  for (let i = 0; i < seats.length; i += 8) {
+    const rowSeats = seats.slice(i, i + 8);
+    rows.push(rowSeats);
+  }
   return (
     <Row>
-      {seats.map((seat, index) => (
-        <Seat
+      {rows.map((seat, index) => (
+        <div
           key={index}
-          size={2}
-          occupied={seat.userName !== "Empty"}
-          setMeta={setMeta}
-          number={seat.userName}
-          seatNumber={seat.seatNumber}
-          meta={meta}
-          orders={orders}
-        />
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginBottom: "10px",
+          }}
+        >
+          <div style={{ display: "flex", marginRight: "30px" }}>
+            {seat.slice(0, 4).map((se, index) => (
+              <Seat
+                key={index}
+                size={2}
+                occupied={se.userName !== "Empty"}
+                setMeta={setMeta}
+                number={se.userName}
+                seatNumber={se.seatNumber}
+                meta={meta}
+                orders={orders}
+              />
+            ))}
+          </div>
+          <div style={{ display: "flex", marginLeft: "30px" }}>
+            {seat.slice(4).map((se, index) => (
+              <Seat
+                key={index}
+                size={2}
+                occupied={se.userName !== "Empty"}
+                setMeta={setMeta}
+                number={se.userName}
+                seatNumber={se.seatNumber}
+                meta={meta}
+                orders={orders}
+              />
+            ))}
+          </div>
+        </div>
       ))}
     </Row>
   );
