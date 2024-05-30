0a5785d996e9f890d490e0a01153fdd3b23b230a
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2024-05-30 10:19:15 +0900

    feat: UserInfo.js - 유저 정보를 관리하는 컴포넌트 분리
    #80

---
+++ src/admin/seatArrange/components/UserInfo.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "../css/UserInfo.css";
+import axios from "axios";
+import Reason from "./Reason";
+
+export default function UserInfo({ orders, setIsClicked, setMessage }) {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const load = async () => {
+      const getUser = await axios.get(`users/orders/${orders}`);
+      setUsers(getUser.data);
+      console.log(getUser.data);
+    };
+    load();
+  }, [orders]);
+
+  return (
+    <Container className="info">
+      <Row className="containerRow">
+        <Row className="infoRow">
+          <Col className="infoCol">번호</Col>
+          <Col className="infoCol">이름</Col>
+          <Col className="infoCol">기수</Col>
+          <Col className="infoCol">앞/뒤</Col>
+          <Col className="infoCol" xs={6}>
+            사유
+          </Col>
+        </Row>
+        {users.map((elem, index) => {
+          return (
+            <Reason
+              elem={elem}
+              index={index}
+              setIsClicked={setIsClicked}
+              setMessage={setMessage}
+            ></Reason>
+          );
+        })}
+      </Row>
+    </Container>
+  );
+}
